b4977af64ef8bb8cdd81441fad41188eb4fbc63f
Humberto Jimenez <jimenez.humbert@gmail.com>
2025-01-01 20:59:42 +1100

    Add files via upload

---
+++ Coding files/run_ml_pipeline.py
@@ -0,0 +1,85 @@
+"""
+This file contains code that will kick off training and testing processes
+"""
+import os
+import json
+import numpy as np
+
+from experiments.UNetExperiment import UNetExperiment
+from data_prep.HippocampusDatasetLoader import LoadHippocampusData
+
+class Config:
+    """
+    Holds configuration parameters
+    """
+    def __init__(self):
+        self.name = "Basic_unet"
+        self.root_dir = r"YOUR DIRECTORY HERE"
+        self.n_epochs = 10
+        self.learning_rate = 0.0002
+        self.batch_size = 8
+        self.patch_size = 64
+        self.test_results_dir = "RESULTS GO HERE"
+
+if __name__ == "__main__":
+    # Get configuration
+
+    # TASK: Fill in parameters of the Config class and specify directory where the data is stored and 
+    # directory where results will go
+    c = Config()
+    c.root_dir = "../data/TrainingSet/"
+    c.test_results_dir = "../out/"
+
+    # Load data
+    print("Loading data...")
+
+    # TASK: LoadHippocampusData is not complete. Go to the implementation and complete it. 
+    data = LoadHippocampusData(c.root_dir, y_shape = c.patch_size, z_shape = c.patch_size)
+
+
+    # Create test-train-val split
+    # In a real world scenario you would probably do multiple splits for 
+    # multi-fold training to improve your model quality
+
+    keys = range(len(data))
+
+    # Here, random permutation of keys array would be useful in case if we do something like 
+    # a k-fold training and combining the results. 
+
+    split = dict()
+
+    # TASK: create three keys in the dictionary: "train", "val" and "test". In each key, store
+    # the array with indices of training volumes to be used for training, validation 
+    # and testing respectively.
+    # using trai_test_split method from https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    def datasplit (keys, trainsp = 0.6, valsp =0.2):
+        keys = np.random.permutation(keys)
+        train_sample = int(trainsp*len(keys))
+        valid_sample = int((trainsp + valsp)*len(keys))
+        test_sample = int((1-trainsp-valsp)*len(keys))
+        return range(0, train_sample), range(train_sample, valid_sample), range(valid_sample, test_sample+valid_sample)
+    
+    split['train'], split['val'], split['test'] = datasplit(keys) 
+    
+    # Set up and run experiment
+    
+    # TASK: Class UNetExperiment has missing pieces. Go to the file and fill them in
+    exp = UNetExperiment(c, split, data)
+
+    # You could free up memory by deleting the dataset
+    # as it has been copied into loaders
+    # del dataset 
+
+    # run training
+    exp.run()
+
+    # prep and run testing
+
+    # TASK: Test method is not complete. Go to the method and complete it
+    results_json = exp.run_test()
+
+    results_json["config"] = vars(c)
+
+    with open(os.path.join(exp.out_dir, "results.json"), 'w') as out_file:
+        json.dump(results_json, out_file, indent=2, separators=(',', ': '))
+
--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-# AI_Healthcare_Hippocampus_Volume_Analyzer
-## Overview
-Creation of an end-to-end AI system that integrates into a clinical-grade viewer and automatically measures hippocampal volumes of new patients to identify Alzheimer disease progression
+# Expected results
 
-## Section 1
-Exploratory Data Analysis: 
-* Inspected a dataset of MRI scans and related segmentations,
-* Represented as NIFTI files, 
-* Visualized image slices,
-* and understood the layout of the data
+Please put the artefacts from Section 2 here:  
+  
+* Functional code that trains the segmentation model
+* Test report with Dice scores on test set (can be a json file). Your final average Dice with the default model should be around .90
+* Screenshots from your Tensorboard (or other visualization engine) output, showing Train and Validation loss plots, along with images of the predictions that your model is making at different stages of training
+* Your trained model PyTorch parameter file (`model.pth`)
 
-## Section 2
-Training of a Segmentation CNN:
-* Functional code for training the segmentation model
-* Test report with Dice scores on test set.
-* Trained model PyTorch parameter file (model.pth)
+## Suggestions for making your project stand out
 
-## Section 3
-Integrating into a Clinical Network:
-* Code that runs inference on a DICOM volume and produces a DICOM report
-* A report.dcm file with a sample report
-* Screenshots of the report shown in the OHIF viewer
-* Validation Plan
+* Can you write a 1-page email explaining what your algorithm is doing to a clinician who will be trying it out, but whom you never met? Make sure you include performance characteristics with some images. Try using their language and think of what would be the important information that they are looking for?
+* Implement additional metrics in the test report such as Jaccard score, sensitivity or specificity. Think of what additional metrics would be relevant.
+* In our dataset we have labels of 2 classes - anterior and posterior segments of the hippocampus. Can you train a version of model that segments the structure as a whole, only using one class? Is the performance better, the same or worse?
+* Write up a short report explaining requirements for your training process (compute, memory) and suggestions for making it more efficient (model architecture, data pipeline, loss functions, data augmentation). What kind of data augmentations would NOT add value?
+* What are best and worst performing volumes? Why do you think that's the case?
